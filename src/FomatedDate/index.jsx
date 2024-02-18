e0bc316b06b8c7abeb844efb90297fafd8657084
Pornpapat <pornpapat.d@ku.th>
2024-02-18 16:22:33 +0700

    Create index.jsx

---
+++ src/FomatedDate/index.jsx
@@ -0,0 +1,10 @@
+const FormatedDate = ({ children }) => {
+  return (
+    <div>
+        <span className={`text-sm ${isMouseEnter['exp1'] ? "text-primaryTitle": ""}`}>
+          {children}
+          </span>
+    </div>
+  )
+}
+export default FormatedDate;
