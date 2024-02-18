9410f82badc404761490270223b959544e6bc8c3
Pornpapat <pornpapat.d@ku.th>
2024-02-18 16:22:53 +0700

    Create index.jsx

---
+++ src/sections/Rightsection/index.jsx
@@ -0,0 +1,16 @@
+import About from "../About";
+import Experience from "../../Experience";
+const Rightsection = () => {
+  return (
+    <div className=' grid gap space-y-36 px-5'>
+        <About/>
+        <Experience/>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+      </div>
+  );
+}
+export default Rightsection
